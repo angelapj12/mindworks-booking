58e861f4ea339949f8ea983251d936e73adcfa0f
Angela Pj <ang@Angelas-Laptop.local>
2025-09-04 20:08:36 +0200

    Set dark background and text colors for site

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="min-h-screen flex flex-col bg-gray-900 text-gray-100">
       <Header />
       <main className="flex-1">
         {children}
